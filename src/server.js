e15f3b27856498a2a05f6f647c1ce8887569f9bd
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-07-23 11:17:27 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,23 +1,59 @@
+'use strict';
+
+const path = require('path');
+const http = require('http');
+
 const RammerheadProxy = require('./classes/RammerheadProxy');
 const RammerheadLogging = require('./classes/RammerheadLogging');
 const RammerheadSession = require('./classes/RammerheadSession');
 const RammerheadSessionAbstractStore = require('./classes/RammerheadSessionAbstractStore');
 const RammerheadSessionFileCache = require('./classes/RammerheadSessionFileCache');
+const RammerheadSessionMemoryStore = require('./classes/RammerheadMemoryStore');
+
 const generateId = require('./util/generateId');
 const addStaticFilesToProxy = require('./util/addStaticDirToProxy');
-const RammerheadSessionMemoryStore = require('./classes/RammerheadMemoryStore');
 const StrShuffler = require('./util/StrShuffler');
 const URLPath = require('./util/URLPath');
 
-module.exports = {
-    RammerheadProxy,
-    RammerheadLogging,
-    RammerheadSession,
-    RammerheadSessionAbstractStore,
-    RammerheadSessionMemoryStore,
-    RammerheadSessionFileCache,
-    StrShuffler,
-    generateId,
-    addStaticFilesToProxy,
-    URLPath
-};
+const PORT = process.env.PORT || 8081;
+
+console.log('Starting Rammerhead proxy server...');
+console.log(`Port: ${PORT}`);
+
+try {
+    // Create the session store
+    const sessionStore = new RammerheadSessionMemoryStore();
+
+    // Create the proxy
+    const proxy = new RammerheadProxy({
+        sessionStore
+    });
+
+    // Create HTTP server
+    const server = http.createServer((req, res) => {
+        proxy.handleRequest(req, res);
+    });
+
+    server.listen(PORT, '0.0.0.0', () => {
+        console.log(`Rammerhead proxy listening on 0.0.0.0:${PORT}`);
+    });
+
+    process.on('SIGTERM', () => {
+        console.log('Received SIGTERM, shutting down...');
+        server.close(() => {
+            process.exit(0);
+        });
+    });
+
+    process.on('SIGINT', () => {
+        console.log('Received SIGINT, shutting down...');
+        server.close(() => {
+            process.exit(0);
+        });
+    });
+
+} catch (error) {
+    console.error('Fatal error during startup:');
+    console.error(error);
+    process.exit(1);
+}
