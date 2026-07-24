7df8a933f4d4a46097bf00a6f0ec2b379c8e150f
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-07-24 20:40:05 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,5 @@
 require('./server/index.js');
+
+// Keep the process alive - the proxy doesn't create persistent event listeners
+// so Node will exit if the event loop has nothing to do
+setInterval(() => {}, 1000);
