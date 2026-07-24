2864780185c30b55309bbc5277331c42f2c67aec
dip0001-boop <dip0001@tarneitcollege.vic.edu.au>
2026-07-24 14:33:29 +1000

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -3,10 +3,6 @@ const gracefulFS = require('graceful-fs');
 
 gracefulFS.gracefulify(fs);
 
-if (require('cluster').isMaster) {
-    require('dotenv-flow').config();
-}
-
 const exitHook = require('async-exit-hook');
 const RammerheadProxy = require('../classes/RammerheadProxy');
 const addStaticDirToProxy = require('../util/addStaticDirToProxy');
@@ -16,7 +12,7 @@ const setupRoutes = require('./setupRoutes');
 const setupPipeline = require('./setupPipeline');
 const RammerheadLogging = require('../classes/RammerheadLogging');
 
-const PORT = Number(process.env.PORT) || config.port || 10000;
+const PORT = Number(process.env.PORT) || 10000;
 const HOST = '0.0.0.0';
 
 const logger = new RammerheadLogging({
@@ -24,24 +20,32 @@ const logger = new RammerheadLogging({
     generatePrefix: config.generatePrefix
 });
 
+/*
+ * IMPORTANT:
+ * dontListen is true because we explicitly start the actual server below.
+ * This prevents Rammerhead's internal listener logic from interfering with
+ * Render's required PORT.
+ */
 const proxyServer = new RammerheadProxy({
     logger,
     loggerGetIP: config.getIP,
     bindingAddress: HOST,
     port: PORT,
     crossDomainPort: null,
-    dontListen: false,
+    dontListen: true,
     ssl: null,
+
     getServerInfo: (req) => {
-        const host = req.headers.host || `${HOST}:${PORT}`;
-        const parsed = new URL(`http://${host}`);
+        const host = req.headers.host || '';
+        const hostname = host.split(':')[0];
 
         return {
-            hostname: parsed.hostname,
-            port: parsed.port ? Number(parsed.port) : PORT,
-            protocol: req.socket.encrypted ? 'https:' : 'http:'
+            hostname,
+            port: 443,
+            protocol: 'https:'
         };
     },
+
     disableLocalStorageSync: config.disableLocalStorageSync,
     diskJsCachePath: config.diskJsCachePath,
     jsCacheSize: config.jsCacheSize
@@ -63,7 +67,24 @@ sessionStore.attachToProxy(proxyServer);
 setupPipeline(proxyServer, sessionStore);
 setupRoutes(proxyServer, sessionStore, logger);
 
-exitHook(() => {
+const server = proxyServer.server1;
+
+if (!server) {
+    throw new Error('Rammerhead did not create its main HTTP server');
+}
+
+server.on('error', (error) => {
+    logger.error(`Server error: ${error.message}`);
+    process.exit(1);
+});
+
+server.listen(PORT, HOST, () => {
+    logger.info(
+        `(server) Rammerhead proxy is listening on http://${HOST}:${PORT}`
+    );
+});
+
+exitHook((done) => {
     logger.info('(server) Received exit signal, closing proxy server');
 
     try {
@@ -73,8 +94,8 @@ exitHook(() => {
     }
 
     logger.info('(server) Closed proxy server');
-});
 
-logger.info(
-    `(server) Rammerhead proxy is listening on http://${HOST}:${PORT}`
-);
+    if (done) {
+        done();
+    }
+});
